4e830db4dcea795da88b9b9d4196a336af7a30ab
ibmarunkarthick <ibmarun94@gmail.com>
2021-07-19 19:38:18 +0530

    first commit

---
+++ src/app/[module-name]/[module-name].module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { [ModuleName]RoutingModule } from './[module-name]-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    [ModuleName]RoutingModule
+  ]
+})
+export class [ModuleName]Module { }
